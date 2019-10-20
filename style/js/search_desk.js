e1d00c999c2b08c0369045aa10a8bbee641c8e1a
Cloner <emadpourjafar@gmail.com>
2019-10-21 00:47:02 +0330

    set date gram on top of page set responsive search box

---
+++ style/js/search_desk.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Cloner on 10/21/2019.
+ */
+
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+
+const expand = () => {
+    searchBtn.classList.toggle("close");
+    input.classList.toggle("square");
+};
+
+searchBtn.addEventListener("click", expand);
