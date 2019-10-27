24d927c18788280196e9d9751562a688cc019f5c
TheMn <mt.lroc@outlook.com>
2019-10-27 21:06:55 +0330

    server settings done templates created

--- helli5/views.py
+++ helli5/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.template import loader
 
 def home_page(request):
-    return HttpResponse("this is home page")
+    template = loader.get_template('index.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
