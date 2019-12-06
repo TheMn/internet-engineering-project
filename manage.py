bc5bc389329121e97736f812f31707f0f0610b5f
mahdi13761376 <mahdi1376ghanbari@gmail.com>
2019-12-06 19:39:52 +0330

    test protection

--- manage.py
+++ manage.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
+	# test
         try:
             import django
         except ImportError:
