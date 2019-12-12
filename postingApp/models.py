a26cbbd165f7c81da3d69adcd98882d92df2290a
asal97 <asal.asgari19@gmail.com>
2019-12-12 13:04:47 +0330

    comment field added to post

--- postingApp/models.py
+++ postingApp/models.py
@@ -14,3 +14,17 @@ class PostStuff(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.text
+
+class Comment(models.Model):
+    post = models.ForeignKey(PostStuff, on_delete = models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    cm_date = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
