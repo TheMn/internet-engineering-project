50d36d96a3c216bd7f6c6e0b0fce709849929ad1
asal97 <asal.asgari19@gmail.com>
2019-12-12 19:54:29 +0330

    img added to user instead of post created attachment table

--- postingApp/admin.py
+++ postingApp/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import PostStuff
+from .models import User
+from .models import Comment
+from .models import Attachment
 # Register your models here.
 admin.site.register(PostStuff)
+admin.site.register(Comment)
+admin.site.register(User)
+admin.site.register(Attachment)
--- postingApp/models.py
+++ postingApp/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='profile')
 
 
 class PostStuff(models.Model):
     title = models.CharField(max_length=100)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=400)
-    img = models.ImageField(upload_to='profile')
-    attachment = models.FileField(upload_to='uploads/%Y/%m/%d/')
     date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.text
+
+class Attachment(models.Model):
+    post = models.ForeignKey(PostStuff, on_delete=models.CASCADE)
+    attach = models.FileField(upload_to='uploads/%Y/%m/%d/')
+
 
 class Comment(models.Model):
     post = models.ForeignKey(PostStuff, on_delete=models.CASCADE)
