14af10253b161737a79434582574a1e578db4a86
asal97 <asal.asgari19@gmail.com>
2019-12-12 11:38:55 +0330

    tiny change in models

--- postingApp/models.py
+++ postingApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class postStuff(models.Model):
     title = models.CharField(max_length=100)
     userName = models.CharField(max_length=50)
-    text = models.CharField(max_length=400)
+    text = models.TextField(max_length=400)
     img = models.ImageField(upload_to='profile')
     attachment = models.FileField(upload_to='uploads/%Y/%m/%d/')
     date = models.DateTimeField(auto_now_add=True)
