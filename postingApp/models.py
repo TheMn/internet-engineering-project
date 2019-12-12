4bba2f6d3db7f1a860f9bb2eafcee20fec189c02
asal97 <asal.asgari19@gmail.com>
2019-12-12 18:13:06 +0330

    comment's author is now an object itself

--- postingApp/models.py
+++ postingApp/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
 
 class PostStuff(models.Model):
     title = models.CharField(max_length=100)
-    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=400)
     img = models.ImageField(upload_to='profile')
     attachment = models.FileField(upload_to='uploads/%Y/%m/%d/')
@@ -16,11 +16,15 @@ class PostStuff(models.Model):
         return self.text
 
 class Comment(models.Model):
-    post = models.ForeignKey(PostStuff, on_delete = models.CASCADE)
-    author = models.CharField(max_length=200)
+    post = models.ForeignKey(PostStuff, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     cm_date = models.DateTimeField(auto_now_add=True)
     approved_comment = models.BooleanField(default=False)
+
+#this below line will allow our comment table to be related to 2 different tables
+    class Meta:
+        unique_together = (('post', 'author'),)
 
     def approve(self):
         self.approved_comment = True
