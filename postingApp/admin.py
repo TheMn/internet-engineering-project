2e176ea2e0298ca4a4e8f28e012dbb876dbe5ea6
asal97 <asal.asgari19@gmail.com>
2019-12-14 19:39:42 +0330

    role based user , extending User django

--- postingApp/admin.py
+++ postingApp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import PostStuff
-from .models import User
+from .models import Profile
 from .models import Comment
 from .models import Attachment
 # Register your models here.
 admin.site.register(PostStuff)
 admin.site.register(Comment)
-admin.site.register(User)
+admin.site.register(Profile)
 admin.site.register(Attachment)
--- postingApp/models.py
+++ postingApp/models.py
@@ -1,19 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=50)
+class Profile(models.Model):
+    ROLE_TEACHER = 1
+    ROLE_STUDENT = 2
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(('email address'), unique=True,default='SOME STRING')
+    STATUS = (
+        (1, ('Teacher')),
+        (2, ('Student')),
+    )
+    status = models.PositiveSmallIntegerField(
+        choices=STATUS,
+        default=1,
+    )
     img = models.ImageField(upload_to='profile')
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    date_joined = models.DateTimeField(('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(('active'), default=True)
+    REQUIRED_FIELDS = [email,user,birth_date]
 
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 class PostStuff(models.Model):
     title = models.CharField(max_length=100)
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.ForeignKey(Profile, on_delete=models.CASCADE)
     text = models.TextField(max_length=400)
     date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.text
+    def __str__(self):
+        return self.username
 
 class Attachment(models.Model):
     post = models.ForeignKey(PostStuff, on_delete=models.CASCADE)
@@ -22,7 +49,7 @@ class Attachment(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(PostStuff, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     text = models.TextField()
     cm_date = models.DateTimeField(auto_now_add=True)
     approved_comment = models.BooleanField(default=False)
