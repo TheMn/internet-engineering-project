f526df2a1ffb384d8aa8f124ae81f54a0dc51c59
asal97 <asal.asgari19@gmail.com>
2019-12-15 13:37:55 +0330

    checkout to new branch

--- postingApp/admin.py
+++ postingApp/admin.py
@@ -3,6 +3,7 @@ from .models import PostStuff
 from .models import Profile
 from .models import Comment
 from .models import Attachment
+
 # Register your models here.
 admin.site.register(PostStuff)
 admin.site.register(Comment)
