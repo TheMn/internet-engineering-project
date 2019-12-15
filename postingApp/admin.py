76d93328c37fe2994a8e818a872cd144bf9b4367
asal97 <asal.asgari19@gmail.com>
2019-12-15 19:15:11 +0330

    jesus this is so messed up

--- postingApp/admin.py
+++ postingApp/admin.py
@@ -3,9 +3,11 @@ from .models import PostStuff
 from .models import Profile
 from .models import Comment
 from .models import Attachment
+from .models import Role
 
 # Register your models here.
 admin.site.register(PostStuff)
 admin.site.register(Comment)
 admin.site.register(Profile)
 admin.site.register(Attachment)
+admin.site.register(Role)
--- postingApp/models.py
+++ postingApp/models.py
@@ -5,18 +5,18 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    ROLE_TEACHER = 1
-    ROLE_STUDENT = 2
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # ROLE_TEACHER = 1
+    # ROLE_STUDENT = 2
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField(('email address'), unique=True,default='SOME STRING')
-    STATUS = (
-        (1, ('Teacher')),
-        (2, ('Student')),
-    )
-    status = models.PositiveSmallIntegerField(
-        choices=STATUS,
-        default=1,
-    )
+    # STATUS = (
+    #     (1, ('Teacher')),
+    #     (2, ('Student')),
+    # )
+    # status = models.PositiveSmallIntegerField(
+    #     choices=STATUS,
+    #     default=1,
+    # )
     img = models.ImageField(upload_to='profile')
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
@@ -27,15 +27,31 @@ class Profile(models.Model):
     def get_first_name(self):
         return self.first_name
 
-    User.add_to_class("__str__", get_first_name)
+    # User.add_to_class("__str__", get_first_name)
 
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
+    # def create_user_profile(sender, instance, created, **kwargs):
+    #     if created:
+    #         Profile.objects.create(user=instance)
+    #
+    # @receiver(post_save, sender=User)
+    # def save_user_profile(sender, instance, **kwargs):
+    #     instance.profile.save()
 
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
+class Role(models.Model):
+    person = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    is_consulter = models.BooleanField(default=False)
+    is_moavenP = models.BooleanField(default=False)
+    is_moavenA = models.BooleanField(default=False)
+    is_moavenE = models.BooleanField(default=False)
+    is_parent = models.BooleanField(default=False)
+    is_principle = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} {} {} {} {} {} {} {}'.format(self.is_student, self.is_teacher, self.is_consulter, self.is_moavenP,
+                                                self.is_moavenA, self.is_moavenE, self.is_principle, self.is_parent)
+
 
 class PostStuff(models.Model):
     title = models.CharField(max_length=100)
