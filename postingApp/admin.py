ca9131f6c25bf09dacf3e5532f3958a32cc78afe
asal97 <asal.asgari19@gmail.com>
2019-12-17 13:05:48 +0330

    ensha'allah last update on user :))))

--- postingApp/admin.py
+++ postingApp/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import PostStuff
+
+from .models import Comment
+from .models import Attachment
+from .models import Role
+from .models import Profile
+
 # Register your models here.
 admin.site.register(PostStuff)
+admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Attachment)
+admin.site.register(Role)
--- postingApp/models.py
+++ postingApp/models.py
@@ -1,34 +1,66 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=50)
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='profilePic')
+    phone = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Role(models.Model):
+    person = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    is_consulter = models.BooleanField(default=False)
+    is_moavenP = models.BooleanField(default=False)
+    is_moavenA = models.BooleanField(default=False)
+    is_moavenE = models.BooleanField(default=False)
+    is_parent = models.BooleanField(default=False)
+    is_principle = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} {} {} {} {} {} {} {}'.format(self.is_student, self.is_teacher, self.is_consulter, self.is_moavenP,
+                                                self.is_moavenA, self.is_moavenE, self.is_principle, self.is_parent)
 
 
 class PostStuff(models.Model):
     title = models.CharField(max_length=100)
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.ForeignKey(Profile, on_delete=models.CASCADE)
     text = models.TextField(max_length=400)
-    img = models.ImageField(upload_to='profile')
-    attachment = models.FileField(upload_to='uploads/%Y/%m/%d/')
     date = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return self.text
+        return '{} {} {} {}'.format(self.username, self.title, self.text, self.date)
+
+
+class Attachment(models.Model):
+    post = models.ForeignKey(PostStuff, on_delete=models.CASCADE)
+    attach = models.FileField(upload_to='uploads/%Y/%m/%d/')
+
 
 class Comment(models.Model):
     post = models.ForeignKey(PostStuff, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    text = models.TextField(max_length=400)
     cm_date = models.DateTimeField(auto_now_add=True)
-    approved_comment = models.BooleanField(default=False)
 
-#this below line will allow our comment table to be related to 2 different tables
     class Meta:
         unique_together = (('post', 'author'),)
-
-    def approve(self):
-        self.approved_comment = True
-        self.save()
-
-    def __str__(self):
-        return self.text
