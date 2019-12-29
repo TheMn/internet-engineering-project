c895bd74acd5ed8c63e12c34fdd42dbd9b534ebf
mahdi13761376 <mahdi1376ghanbari@gmail.com>
2019-12-29 13:25:51 +0330

    change wsgi file

--- helli5/wsgi.py
+++ helli5/wsgi.py
@@ -1,16 +1,22 @@
-"""
-WSGI config for helli5 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
 import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helli5.settings')
-
-application = get_wsgi_application()
+import time 
+import traceback 
+import signal 
+import sys 
+ 
+from django.core.wsgi import get_wsgi_application 
+ 
+sys.path.append('/var/www/vhosts/mysite') 
+# adjust the Python version in the line below as needed 
+sys.path.append('/var/www/vhosts/mysite/venv/lib/python3.5/site-packages') 
+ 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings") 
+ 
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5) 
