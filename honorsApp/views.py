464b7c8e4d25187f9d67f50f5934f8919d699f0c
themn <mt.lroc@outlook.com>
2020-01-14 12:12:07 +0330

    create honors view

--- honorsApp/views.py
+++ honorsApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Honors
+
 
 # Create your views here.
+
+def honors(request):
+    honors_list = Honors.objects.all()
+    context = {
+        'all_honors': honors_list
+    }
+    return render(request, 'honors.html', context)
