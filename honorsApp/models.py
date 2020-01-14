ff7102e1e9c25605407b22be37a703e7ee2cb0ad
themn <mt.lroc@outlook.com>
2020-01-14 17:55:51 +0330

    add category in honors template, TODO: add period

--- honorsApp/models.py
+++ honorsApp/models.py
@@ -5,6 +5,15 @@ from postingApp.models import Category
 # Create your models here.
 class Honors(models.Model):
     name = models.CharField(max_length=50)
+    title = models.CharField(max_length=150)
     avatar = models.ImageField(upload_to="honors")
     categories = models.ManyToManyField(Category)
     period = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def get_class(self):
+        category = " ".join([cat.title for cat in self.categories.all()])
+        return ''.join(["element-item col-md-6 col-lg-4 ", category])
