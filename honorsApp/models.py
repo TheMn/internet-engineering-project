4c668a721e4b27f2c8b2bfb14253623b4f612027
themn <mt.lroc@outlook.com>
2020-01-15 20:48:42 +0330

    limit category choices, fix bugs, and now search is f*cked up :/

--- honorsApp/models.py
+++ honorsApp/models.py
@@ -5,8 +5,8 @@ from datetime import datetime as dt
 
 def period_choices():
     school_start_year = 2011
-    periods = ["دوره "+str(x - school_start_year + 1) for x in range(school_start_year, dt.now().year)]
-    return tuple([list(x) for x in zip(range(len(periods)), periods)])
+    periods = ["دوره " + str(x - school_start_year + 1) for x in range(school_start_year, dt.now().year)]
+    return [x for x in zip(periods, periods)]
 
 
 # Create your models here.
@@ -14,7 +14,7 @@ class Honors(models.Model):
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=150, blank=True)
     avatar = models.ImageField(upload_to="honors")
-    categories = models.ManyToManyField(Category)
+    categories = models.ManyToManyField(Category, limit_choices_to={'is_honored': True})
     period = models.CharField(max_length=50, choices=period_choices())
 
     def __str__(self):
@@ -23,4 +23,4 @@ class Honors(models.Model):
     @property
     def get_class(self):
         category = " ".join([cat.title for cat in self.categories.all()])
-        return ''.join([self.period, " element-item col-md-6 col-lg-4 ", category])
+        return " ".join([self.period, category])
