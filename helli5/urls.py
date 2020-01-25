300b80d1573a85307e5de0e7af1f91ac8748eeec
mahdi13761376 <mahdi1376ghanbari@gmail.com>
2020-01-26 00:21:36 +0330

    farsilization

--- helli5/urls.py
+++ helli5/urls.py
@@ -28,15 +28,15 @@ urlpatterns = [
 
   path('', views.index, name='index'),
   path('admin/', admin.site.urls),
-  path('contact/', views.contact, name='contact'),
-  path('about/', views.about, name='about'),
+  path('ارتباط-با-ما/', views.contact, name='contact'),
+  path('درباره-ما/', views.about, name='about'),
 
   # path('error503/', views.error503, name='error503'),
   path('djrichtextfield/', include('djrichtextfield.urls')),
 
-  url(r'^accounts/', include('loginApp.urls')),
-  url(r'^blog/', include('postingApp.urls')),
+  url(r'^حساب-ها/', include('loginApp.urls')),
+  url(r'^نوشته-ها/', include('postingApp.urls')),
   url(r'^honors/', include('honorsApp.urls')),
-  url(r'^courses/', include('courseApp.urls')),
+  # url(r'^courses/', include('courseApp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- loginApp/urls.py
+++ loginApp/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from loginApp import views
 
 urlpatterns = [
-    url(r'login/', views.login, name='login'),
+    url(r'ورود/', views.login, name='login'),
     # by default login will redirect user to /accounts/profile
-    url(r'logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    url(r'خروج/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
