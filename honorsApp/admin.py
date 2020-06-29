458ccda9358b2a0fc1dd2952757676993f24b7c7
themn <mt.lroc@outlook.com>
2020-06-30 01:51:43 +0430

    honors admin improved

--- honorsApp/admin.py
+++ honorsApp/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import *
 
+
+@admin.register(Honors)
+class HonorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title', 'title_2', 'period',)
+    list_editable = ('title', 'title_2',)
+    list_filter = ('period',)
+    search_fields = ('name',)
 # Register your models here.
-admin.site.register(Honors)
+# admin.site.register(Honors)
