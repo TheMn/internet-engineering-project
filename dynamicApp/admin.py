ba94eac638590cdbd4932adb5a56ff4c8aaeed45
themn <mt.lroc@outlook.com>
2020-07-02 19:13:42 +0430

    improve admin style

--- dynamicApp/admin.py
+++ dynamicApp/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import SliderContent
-# Register your models here.
-admin.site.register(SliderContent)
+
+
+@admin.register(SliderContent)
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ('date', 'title', 'description', 'visible')
+    search_fields = ('title',)
+    list_filter = ('visible',)
+    list_editable = ('title', 'description', 'visible')
