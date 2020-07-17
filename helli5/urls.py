debbd9c38ff3c537121db8634a2c897da3ba151c
themn <mt.lroc@outlook.com>
2020-07-17 23:55:49 +0430

    add 400, 403, 404, and 500 handler and set debug = False

--- helli5/urls.py
+++ helli5/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include, handler404
+from django.conf.urls import url, include, handler400, handler403, handler404, handler500
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -25,26 +25,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from postingApp.feeds import LatestPostsFeed
 
-# handler404 = views.custom_404
+handler400 = views.custom_400
+handler403 = views.custom_403
+handler404 = views.custom_404
+handler500 = views.custom_500
 
 urlpatterns = [
 
-  path('', views.index, name='index'),
-  path('404/', views.custom_404, name='404'),
-  path('admin/', admin.site.urls),
-  path('feed/rss', LatestPostsFeed(), name='posts_feed'),
-  path('ارتباط-با-ما/', views.contact, name='contact'),
-  path('درباره-ما/', views.about, name='about'),
-  path('دبیران/', views.teachers, name='teachers'),
+                  path('', views.index, name='index'),
+                  path('admin/', admin.site.urls),
+                  path('feed/rss', LatestPostsFeed(), name='posts_feed'),
+                  path('ارتباط-با-ما/', views.contact, name='contact'),
+                  path('درباره-ما/', views.about, name='about'),
+                  path('دبیران/', views.teachers, name='teachers'),
 
-  # path('error503/', views.error503, name='error503'),
-  # path('djrichtextfield/', include('djrichtextfield.urls')),
-  url(r'^tinymce/', include('tinymce.urls')),
+                  # path('error503/', views.error503, name='error503'),
+                  # path('djrichtextfield/', include('djrichtextfield.urls')),
+                  url(r'^tinymce/', include('tinymce.urls')),
 
-  url(r'^حساب-ها/', include('loginApp.urls')),
-  url(r'^نوشته-ها/', include('postingApp.urls')),
-  url(r'^افتخارات/', include('honorsApp.urls')),
-  url(r'panel/', include('smsApp.urls')),
-  url(r'^courses/', include('courseApp.urls')),
+                  url(r'^حساب-ها/', include('loginApp.urls')),
+                  url(r'^نوشته-ها/', include('postingApp.urls')),
+                  url(r'^افتخارات/', include('honorsApp.urls')),
+                  url(r'panel/', include('smsApp.urls')),
+                  url(r'^courses/', include('courseApp.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- helli5/views.py
+++ helli5/views.py
@@ -96,8 +96,17 @@ def teachers(request):
     return render(request, 'teachers.html', context)
 
 
-def custom_404(request):
+def custom_400(request, exception):
+    return render(request, '400.html', status=400)
+
+
+def custom_403(request, exception):
+    return render(request, '403.html', status=403)
+
+
+def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
-# def error503(request):
-#     return render(request, 'error503.html', {})
+
+def custom_500(request, exception):
+    return render(request, '500.html', status=500)
