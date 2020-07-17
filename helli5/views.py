98a890579b6a873a4021eff3764fbd72ccaf45a1
themn <mt.lroc@outlook.com>
2020-07-17 23:58:20 +0430

    set number of 500 error arguments to 1 TODO: check from doc

--- helli5/views.py
+++ helli5/views.py
@@ -108,5 +108,5 @@ def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
 
-def custom_500(request, exception):
+def custom_500(request):
     return render(request, '500.html', status=500)
