9c488ee7ecfcae9187a4af6bb159d65dd0b338ad
mahdi <mahdi1376ghanbari@gmail.com>
2020-12-12 10:11:00 +0330

    change sort

--- eLearning/views.py
+++ eLearning/views.py
@@ -160,7 +160,8 @@ def check_classes(request):
                     adobe_students = tmp
             elif cls == 'webinar':
                 adobe_students = []
-                all_students = User.objects.filter(username__regex='9d*').all()
+                all_students = User.objects.filter(username__regex='97*').all() | User.objects.filter(
+                    username__regex='98*').all() | User.objects.filter(username__regex='99*').all()
                 response = requests.get(
                     'https://online.allamehelli5.ir/api/xml?action=report-meeting-attendance&sco-id=' + classes.get(
                         'webinar') + '&' + generate_date_query_param(zang_start, zang_end) + '&session=' + breeze)
