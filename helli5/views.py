7a7336c04c83d2f4218bd7dee77cbe6ba46f32e3
mahdi <mahdi1376ghanbari@gmail.com>
2021-05-31 08:37:01 +0430

    export users

--- helli5/views.py
+++ helli5/views.py
@@ -11,6 +11,8 @@ from django.shortcuts import render, redirect
 from .forms import BunchAddForm
 from django.http import HttpResponse
 import xlrd
+import xlwt
+from django.contrib.auth.models import User
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -170,3 +172,34 @@ def bunch_add_model(request):
         }
         return render(request, 'bunch_add.html', context)
     return HttpResponse(401)
+
+
+def export(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="Users.xls"'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Users')
+
+    # Sheet header, first row
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = ['Username', 'First name', 'Last name', 'Email address']
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+
+    rows = User.objects.all().values_list('username', 'first_name', 'last_name', 'email')
+    for row in rows:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+    return response
