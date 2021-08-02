c3f6e646e8130afda8889564a06606c9d092c736
mahdi <mahdi1376ghanbari@gmail.com>
2021-08-02 09:40:35 +0430

    debuging

--- eLearning/views.py
+++ eLearning/views.py
@@ -66,8 +66,6 @@ def check_classes(request):
                 response = requests.get(
                     'https://online.allamehelli5.ir/api/xml?action=report-meeting-attendance&sco-id=' + classes.get(
                         '112') + '&' + generate_date_query_param(zang_start, zang_end) + '&session=' + breeze)
-                print('https://online.allamehelli5.ir/api/xml?action=report-meeting-attendance&sco-id=' + classes.get(
-                    '112') + '&' + generate_date_query_param(zang_start, zang_end) + '&session=' + breeze)
                 tmp = adobe_students
                 try:
                     response = response.content
