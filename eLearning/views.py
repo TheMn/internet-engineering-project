50dbedcc44ad961098c45adfbf4d7b17addfc43f
mahdi <mahdi1376ghanbari@gmail.com>
2021-08-02 08:54:14 +0430

    debuging

--- eLearning/views.py
+++ eLearning/views.py
@@ -199,7 +199,7 @@ def check_classes(request):
             emails = {}
             for adobe_student in adobe_students:
                 if adobe_student['results']['report-meeting-attendance'] is not None:
-                    print('salam')
+                    print(['results']['report-meeting-attendance'])
                     for student in adobe_student['results']['report-meeting-attendance']['row']:
                         if isinstance(student, dict):
                             date_end = 'todayT' + end_times[zang] + '.'  # to do split with "T" character
