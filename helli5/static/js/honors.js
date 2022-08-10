cc18891832a6c64c7c1ade89e7fcaca4367e4f9d
Hpsein Khansari <hrk.x11@gmail.com>
2022-08-10 14:11:32 +0430

    [honors] search in the descriptions too

--- helli5/static/js/honors.js
+++ helli5/static/js/honors.js
@@ -1,62 +1,3 @@
-// // quick search regex
-// var qsRegex;
-// var buttonFilter;
-// var buttonFilter2;
-
-// // init Isotope
-// var $grid = $('.grid').isotope({
-//   itemSelector: '.element-item',
-//   layoutMode: 'fitRows',
-//   filter: function() {
-//     var $this = $(this);
-//     var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
-//     var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
-//     var buttonResult2 = buttonFilter2 ? $this.is( buttonFilter2 ) : true;
-//     return searchResult && buttonResult2 && buttonResult ;
-//   }
-// });
-
-// // bind filter on select change
-// $('.filters-select').on( 'change', function() {
-//   // get filter value from option value
-//   // var filterValue = this.value;
-//   // use filterFn if matches value
-//   buttonFilter = this.value;
-//   //$grid.isotope({ filter: filterValue });
-//   $grid.isotope();
-// });
-
-// // bind filter on select change
-// $('.filters-select2').on( 'change', function() {
-//   // get filter value from option value
-//   // var filterValue = this.value;
-//   // use filterFn if matches value
-//   buttonFilter2 = this.value;
-//   //$grid.isotope({ filter: filterValue });
-//   $grid.isotope();
-// });
-
-// // use value of search field to filter
-// var $quicksearch = $('#quicksearch').keyup( debounce( function() {
-//   qsRegex = new RegExp( $quicksearch.val(), 'gi' );
-//   $grid.isotope();
-// }) );
-
-// // debounce so filtering doesn't happen every millisecond
-// function debounce( fn, threshold ) {
-//   var timeout;
-//   return function debounced() {
-//     if ( timeout ) {
-//       clearTimeout( timeout );
-//     }
-//     function delayed() {
-//       fn();
-//       timeout = null;
-//     }
-//     setTimeout( delayed, threshold || 100 );
-//   };
-// }
-
 /** @type {HTMLSelectElement} */
 const subjectSelect = document.getElementById('firstSelector')
 /** @type {HTMLSelectElement} */
@@ -67,17 +8,19 @@ const notFound = document.getElementById('not-found')
 const items = document.querySelectorAll('.item')
 
 function filter() {
-  const period = periodSelect.selectedIndex // zero if nothing is selected
+  const selectedPeriodNumber = periodSelect.selectedIndex // zero if nothing is selected
   const subject = subjectSelect.value // empty string if nothing is selected
   const searchQuery = searchInput.value
   
   let found = false
   for (const item of items) {
-    const itemName = item.getAttribute('data-name')
-    const itemPeriodNumber = parseInt(item.getAttribute('data-period').match(/\d+/)[0])
+    const itemName = item.querySelector('.honor-name').textContent
+    const itemPeriod = item.querySelector('.honor-period').textContent
+    const itemText = item.querySelector('.honor-text').textContent
+    const itemPeriodNumber = parseInt(itemPeriod.match(/\d+/)[0])
     if (
-      (!period || period === itemPeriodNumber) &&
-      (!searchQuery || itemName.includes(searchQuery)) &&
+      (!selectedPeriodNumber || selectedPeriodNumber === itemPeriodNumber) &&
+      (!searchQuery || itemName.includes(searchQuery) || itemText.includes(searchQuery)) &&
       (!subject || item.classList.contains(subject))
     ) {
       item.classList.remove('d-none')
